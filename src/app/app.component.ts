73febcd2fd8bccdbd798a2283e9537ca5b029827
daniel ra <winra001@hotmail.com>
2019-01-05 21:36:22 +1100

    15-188 Setting and Patching Form Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,21 @@ export class AppComponent {
 
     suggestUserName() {
         const suggestedName = 'Superuser';
+
+        // this.signupForm.setValue({
+        //     userData: {
+        //         username: suggestedName,
+        //         email: ''
+        //     },
+        //     secret: 'pet',
+        //     questionAnswer: '',
+        //     gender: 'male'
+        // });
+        this.signupForm.form.patchValue({
+            userData: {
+                username: suggestedName
+            }
+        });
     }
 
     // onSubmit(form: NgForm) {
